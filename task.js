22026bc6df5ffaaad1eee23fdc6904fb690f5b2f
Raj-Kishore-Acharya <94112198+Raj-Kishore-Acharya@users.noreply.github.com>
2022-03-29 19:56:47 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,21 @@
+
+async function info() {
+    const result =await fetch('db.json')
+    const name = await result.json()
+    console.log(name);
+}
+
+info()
+
+
+async function info1() {
+    const result =await fetch('db1.json')
+    const name = await result.json()
+    console.log(name);
+}
+
+
+
+
+
+
